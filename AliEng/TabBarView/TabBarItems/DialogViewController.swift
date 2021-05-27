b664bce23665e87d25bd0bc52b9e72e44ef41c0a
UmidTalibov <umid.izamovich@gmail.com>
2021-05-27 10:34:13 +0500

    add Dialog tableView

--- AliEng/TabBarView/TabBarItems/DialogViewController.swift
+++ AliEng/TabBarView/TabBarItems/DialogViewController.swift
@@ -7,23 +7,52 @@
 
 import UIKit
 
-class DialogViewController: UIViewController {
-
+class DialogViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    var selectedTheme = ""
+    var titleArray:[String] = []
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        defaultSettings()
+       
+    }
+    func defaultSettings(){
+       tableView.separatorColor = .clear
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib.init(nibName: "DetailTableCell", bundle: nil), forCellReuseIdentifier: "detail")
+        titleArray = ["Лекция","Сабак сездeрi","Практика","Диалог"]
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+        return titleArray.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "detail", for: indexPath) as! DetailTableCell
+      
+        cell.nameLabel.text = titleArray[indexPath.row]
+            return cell
+        }
+       
+       
 }
